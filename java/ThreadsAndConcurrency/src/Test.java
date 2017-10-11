bb2079ea366d34c1b71b603fac6c06d0f6918c7d
wujiaming <1114802868@qq.com>
2017-10-11 11:33:17 +0800

    first thread

--- java/ThreadsAndConcurrency/src/Test.java
+++ java/ThreadsAndConcurrency/src/Test.java
@@ -1,5 +1,36 @@
-public class Test {
+import java.lang.Thread;
+import java.lang.Runnable;
+
+class Call {
+    public Runnable r;
+    public Call() {
+        r = () -> {
+            System.out.println("Hello from thread");
+        };
+    }
+
+    public void test(){
+        Thread t = new Thread(r,"thread-1");
+        t.start();
+        System.out.println(t.getName());
+        System.out.println(t.getName());
+        System.out.println(t.getName());
+        try{
+            Thread.sleep(3000);
+        }catch (Exception e){
+            System.out.println("Error");
+        }
+        System.out.println(t.getName());
+        System.out.println(t.getName());
+        System.out.println(t.getName());
+        System.out.println(t.isAlive());
+
+    }
+}
+
+public class Test{
     public static void main(String[] argv){
-        System.out.println("Hello World");
+        Call call = new Call();
+        call.test();
     }
 }
