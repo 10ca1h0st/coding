f912c47c1f7bac12ebe706e04c6fa6d2cf1eaa4d
wujiaming <1114802868@qq.com>
2017-10-11 14:20:56 +0800

    "改进java"

--- java/ThreadsAndConcurrency/src/Test.java
+++ java/ThreadsAndConcurrency/src/Test.java
@@ -7,27 +7,36 @@ class Call {
     public Runnable r;
     public Call() {
         r = () -> {
-            System.out.println("Hello from thread");
+            Thread me = Thread.currentThread();
+            System.out.println("Hello from thread:"+me.getName());
+            //当设置了守护线程后，只要主线程结束，就退出，而不是继续沉睡
+            /*try{
+                Thread.sleep(100000);
+            }catch (Exception e){
+                System.out.println("Error");
+            }*/
         };
     }
 
     public void test(){
         Thread t = new Thread(r,"thread-1");
         System.out.println("优先级:"+t.getPriority());
-        System.out.println("最高优先级:"+Thread.MAX_PRIORITY+" 最小优先级:"+Thread.MIN_PRIORITY);
+        System.out.println("最高优先级:"+Thread.MAX_PRIORITY+" 最小优先级:"+Thread.MIN_PRIORITY+" 默认优先级:"+Thread.NORM_PRIORITY);
+        System.out.println("线程是否为守护线程:"+t.isDaemon());
         System.out.println("线程的状态："+t.getState());
         System.out.println("线程是否存活:"+t.isAlive());
+        //t.setDaemon(true);
         t.start();
         System.out.println("线程的状态："+t.getState());
         System.out.println("线程是否存活:"+t.isAlive());
-        System.out.println(t.getName());
+        System.out.println("线程的名字:"+t.getName());
         System.out.println("沉睡一会");
         try{
             Thread.sleep(2000);
         }catch (Exception e){
             System.out.println("Error");
         }
-        System.out.println(t.getName());
+        System.out.println("线程的名字:"+t.getName());
         System.out.println("线程的状态："+t.getState());
         System.out.println("线程是否存活:"+t.isAlive());
 
@@ -35,7 +44,7 @@ class Call {
 }
 
 public class Test{
-    public static void main(String[] argv){
+    public static void main(String[] args){
         System.out.println("可用的处理器数量:"+Runtime.getRuntime().availableProcessors());
         Call call = new Call();
         call.test();
