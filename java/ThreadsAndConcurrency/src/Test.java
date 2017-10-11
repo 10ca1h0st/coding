b8f910b059b8967e6bc582ca49ef252e2a09b113
wujiaming <1114802868@qq.com>
2017-10-11 13:04:36 +0800

    "改进java"

--- java/ThreadsAndConcurrency/src/Test.java
+++ java/ThreadsAndConcurrency/src/Test.java
@@ -1,5 +1,7 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.lang.Thread;
 import java.lang.Runnable;
+
 
 class Call {
     public Runnable r;
@@ -11,7 +13,11 @@ class Call {
 
     public void test(){
         Thread t = new Thread(r,"thread-1");
+        System.out.println(t.getState());
+        System.out.println(t.isAlive());
         t.start();
+        System.out.println(t.isAlive());
+        System.out.println(t.getState());
         System.out.println(t.getName());
         System.out.println(t.getName());
         System.out.println(t.getName());
