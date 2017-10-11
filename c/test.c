293abe50fe111ecf0879a69a96fa4097d9af86c8
wujiaming <1114802868@qq.com>
2017-10-11 09:19:19 +0800

    线程与并发

--- c/test.c
+++ c/test.c
@@ -1,16 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(int argc,char *argv[]){
-    printf("hello world\n");
-    if(argc > 1){
-        printf(argv[1]);
-        printf("\n");
-    }
-    char *a = "ni hao";
-    printf(a);
-    int b = 10;
-    printf("\n");
-    printf("%d",b);
-    printf("\n");
+int main(int argc,char* argv[]){
+    printf("Hello World\n");
     return 0;
 }
