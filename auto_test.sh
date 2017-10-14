e4c7822c78a56fc10803c3d859906f504bf63ab2
wujiaming <1114802868@qq.com>
2017-10-14 20:52:32 +0800

    优化脚本

---
+++ auto_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir_full=`pwd`
+dir=`basename $dir_full`
+commit=$1
+passwd=$2
+git add .
+git commit -m"$commit"
+/usr/bin/expect << EOF
+set timeout 30
+spawn git push https://github.com/wujiaming123/${dir}.git master
+expect "'https://github.com':"
+send "wujiaming123\n"
+expect "'https://wujiaming123@github.com':"
+send "${passwd}\n"
+expect eof
+EOF
